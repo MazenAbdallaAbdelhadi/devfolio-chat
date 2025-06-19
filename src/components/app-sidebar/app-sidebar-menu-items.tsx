798fa85945efff23e973606387c60c4a17882865
Mazen Abdalla <92177398+MazenAbdallaAbdelhadi@users.noreply.github.com>
2025-06-19 21:57:20 +0300

    Phase 3: add chat share & edit & delete buttons

--- src/components/app-sidebar/app-sidebar-menu-items.tsx
+++ src/components/app-sidebar/app-sidebar-menu-items.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 import {
   SidebarGroupContent,
   SidebarMenu,
+  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Edit3Icon, EllipsisIcon, ShareIcon, Trash2Icon } from "lucide-react";
 
 const items = [
   {
@@ -36,8 +39,33 @@ export const AppSidebarMenuItems = () => {
         {items.map((item, i) => (
           <SidebarMenuItem key={i}>
             <SidebarMenuButton>
-              <Link href={item.href} className="text-nowrap">{item.label}</Link>
+              <Link href={item.href} className="text-nowrap truncate">
+                {item.label}
+              </Link>
             </SidebarMenuButton>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuAction>
+                  <EllipsisIcon />
+                </SidebarMenuAction>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <ShareIcon/>
+                  <span>Share</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Edit3Icon/>
+                  <span>Edit</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem variant="destructive">
+                  <Trash2Icon/>
+                  <span>Delete</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SidebarMenuItem>
         ))}
       </SidebarMenu>
